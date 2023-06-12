189f1f219f5213ed9a7aa57bff17599ac1fe646b
uezili <weslmaciel@gmail.com>
2023-06-12 20:52:43 -0300

    Implementation of monitor

--- classes/main.py
+++ classes/main.py
@@ -1,32 +1,27 @@
-from Carros import Carro
-from Ponte import Ponte
+from ponte import Ponte
+from carros import Carro
 
-n = 50
-Ta_min = 1
-Ta_max = 3
-Tc = 5
-Ts = 1
+N = 100
+TC = 5
+P = 5
 
-ponte = Ponte()
+ponte = Ponte(P)
 
 carros = []
-for i in range(n):
-    sentido = "ida" if i < n/2 else "volta"
+for i in range(N):
+    sentido = "ida" if i < N / 2 else "volta"
     carros.append(Carro(i+1, sentido, ponte))
     carros[i].start()
 
 for carro in carros:
     carro.join()
 
-tempo_medio_espera = ponte.tempo_espera_total / n
+tempo_medio_espera = ponte.tempo_espera_total / N
 tempo_utilizacao_ponte = ponte.tempo_utilizacao_ponte / ponte.tempo_total
 
-print(
-    f"Quantidade de carros que cruzaram no sentido ida: {ponte.carros_cruzados_ida}")
-print(
-    f"Quantidade de carros que cruzaram no sentido volta: {ponte.carros_cruzados_volta}")
+print(f"Quantidade de carros que cruzaram no sentido ida: {ponte.carros_cruzados_ida}")
+print(f"Quantidade de carros que cruzaram no sentido volta: {ponte.carros_cruzados_volta}")
 print(f"Tempo mínimo de espera na fila: {ponte.tempo_espera_total} segundos")
-print(
-    f"Tempo máximo de espera na fila: {ponte.tempo_espera_total + Tc} segundos")
+print(f"Tempo máximo de espera na fila: {ponte.tempo_espera_total + TC} segundos")
 print(f"Tempo médio de espera na fila: {tempo_medio_espera} segundos")
-print(f"Tempo de utilização da ponte: {tempo_utilizacao_ponte * n:.2f}%")
+print(f"Tempo de utilização da ponte: {tempo_utilizacao_ponte * N:.2f}%")
--- classes/ponte.py
+++ classes/ponte.py
@@ -1,49 +1,49 @@
 import threading
 import time
 
-Tc = 5
-Ta_min = 1
-Ta_max = 3
+
+TA_MAX = 3
+TC = 5
 
 
 class Ponte:
-    def __init__(self):
+    def __init__(self, P):
         self.max_carros_na_ponte = 5
         self.carros_na_ponte = 0
-        self.mutex = threading.Semaphore(1)
+        self.mutex = threading.Lock()
+        self.condition = threading.Condition(self.mutex)
         self.carros_cruzados_ida = 0
         self.carros_cruzados_volta = 0
         self.tempo_espera_total = 0
         self.tempo_utilizacao_ponte = 0
         self.tempo_total = 0
+        self.P = P
+        self.carros_atravessados = 0
+        self.sentido_atual = "ida"
 
     def atravessar(self, carro):
         self.mutex.acquire()
 
-        while self.carros_na_ponte >= self.max_carros_na_ponte:
-            self.mutex.release()
-            time.sleep(0.1)
-            self.mutex.acquire()
+        while self.carros_na_ponte >= self.max_carros_na_ponte or self.sentido_atual != carro.sentido:
+            self.condition.wait()
 
         self.carros_na_ponte += 1
+        self.carros_atravessados += 1
+        self.condition.notifyAll()
         self.mutex.release()
 
         if carro.sentido == "ida":
-            print(
-                f"Carro -> {threading.current_thread().name} atravessando a ponte no sentido {carro.sentido}")
             tempo_inicial = time.time()
-            time.sleep(Tc)
+            time.sleep(TC)
             tempo_final = time.time()
             print(
-                f"Carro -> {threading.current_thread().name} atravessou a ponte no sentido {carro.sentido}")
+                f"Carro -> {carro.identificador} atravessou a ponte no sentido {carro.sentido}")
         else:
-            print(
-                f"Carro <- {threading.current_thread().name} atravessando a ponte no sentido {carro.sentido}")
             tempo_inicial = time.time()
-            time.sleep(Tc)
+            time.sleep(TC)
             tempo_final = time.time()
             print(
-                f"Carro <- {threading.current_thread().name} atravessou a ponte no sentido {carro.sentido}")
+                f"Carro <- {carro.identificador} atravessou a ponte no sentido {carro.sentido}")
 
         self.mutex.acquire()
         self.carros_na_ponte -= 1
@@ -51,8 +51,12 @@ class Ponte:
             self.carros_cruzados_ida += 1
         else:
             self.carros_cruzados_volta += 1
-        self.tempo_espera_total += tempo_inicial - \
-            (carro.identificador * Ta_max)
+        self.tempo_espera_total += tempo_inicial - (carro.identificador * TA_MAX)
         self.tempo_utilizacao_ponte += tempo_final - tempo_inicial
-        self.tempo_total += tempo_final - (carro.identificador * Ta_max)
+        self.tempo_total += tempo_final - (carro.identificador * TA_MAX)
+
+        if self.carros_atravessados >= self.P:
+            self.sentido_atual = "volta" if self.sentido_atual == "ida" else "ida"
+            self.carros_atravessados = 0
+        self.condition.notifyAll()
         self.mutex.release()
